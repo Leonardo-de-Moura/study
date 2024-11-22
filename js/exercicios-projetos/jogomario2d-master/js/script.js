123730ada97b7172b4fc898bcc91383495cdcc55
leonardo-de-moura <francisco.tico.fssm@gmail.com>
2024-11-22 10:33:26 -0300

    atualizaçao projeto2d

--- js/exercicios-projetos/jogomario2d-master/js/script.js
+++ js/exercicios-projetos/jogomario2d-master/js/script.js
@@ -14,15 +14,15 @@ const loop = setInterval(() => {
 
 
     if ((pipeposition <= 120 ) && (pipeposition > 0) && (marioposition < 80)) { 
- 
+
         pipe.style.animation='none';
         pipe.style.left = `${pipeposition}px`;
 
         mario.style.animation='none';
         mario.style.bottom = `${marioposition}px`;
 
-        mario.src= '..//imagens/game-over.png';
-        mario.style.width = '75px'; 
+        mario.src= 'https://spng.pngfind.com/pngs/s/685-6857393_shrek-face-png-shrek-forever-after-transparent-png.png';
+        mario.style.width = '200px'; 
         mario.style.marginLeft = '50px';
         clearInterval(loop);
     }   
